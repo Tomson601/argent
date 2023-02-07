284244bd3428c1f671e60c2b4a6a780fe0d5b0de
Tomasz Jarko <tjarko@man.poznan.pl>
2023-02-07 16:42:09 +0100

    Fixing 404 response site

--- src/argent.py
+++ src/argent.py
@@ -13,9 +13,6 @@ def __get_route(request):
 
 
 def __send_respone(client, code, headers, response):
-    print("\n")
-    print(type(headers), headers)
-    print(type(response), response)
     client.send(headers)
     client.send(response)
     client.close()
@@ -37,6 +34,8 @@ def __create_url_linker(url, methods, function):
 
 
 def __get_route_function(route):
+    function = None
+
     for entry in url_linker:
         if entry["route"] == route:
             function = entry["function"]
@@ -51,23 +50,15 @@ def listen(socket):
     request = client.recv(BUFFER_SIZE)
 
     route = __get_route(request)
-    
-    print(route)
-    
+
     function = __get_route_function(route)
-    print(function)
+
     if function != None:
         code, headers, response = function(request)
         __send_respone(client, code, http_headers, response)
     else:
         __send_respone(client, 404, http_headers, "ERROR 404, not found")
-    # socket.close()
 
 
 def route(url, methods='GET'):
     return lambda function: __create_url_linker(url, methods, function)
-
-# @route("/hello/world")
-# def hello_world_url(request):
-#     return(200, {}, "Hello world!")
-
